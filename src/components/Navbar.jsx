5dce66d694ecab229884fba5a8129d60656613aa
Your Name <you@example.com>
2018-08-03 12:17:26 +0300

    second changes of proeject

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+   
+    render() { 
+        return <div>
+            <nav class="navbar navbar-light bg-light">
+  <span class="navbar-brand mb-0 h1">Navbar</span>
+</nav>
+     <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
+  
+
+        </div>;
+    }
+}
+ 
+export default Navbar;
